44dab8b25157cb3dc6812753d4dd69454989efdf
Googlovsk <lgor2000cch@gmail.com>
2024-06-18 20:16:29 +0300

    Initial Commit

---
+++ DisplayManager/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace DisplayManager
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            Forms.NotifyIcon notifyIcon = new Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("/Resources/display.png");
+            notifyIcon.Visible = true;
+
+            base.OnStartup(e);
+        }
+    }
+
+}
